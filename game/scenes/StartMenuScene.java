079ebe1275aec2c04ce67ed56537de3f1b1b845a
iLenny <Leibniz.berihuete@gmail.com>
2016-06-01 21:09:08 -0400

    StartMenuScene fixed

--- game/scenes/StartMenuScene.java
+++ game/scenes/StartMenuScene.java
@@ -1,5 +1,34 @@
 package game.scenes;
 
-public class StartMenuScene {
+import game.scenes.layouts.IntroLayout;
+import game.scenes.layouts.StartMenuLayout;
+import javafx.scene.Scene;
 
+/**
+ * 
+ * @author Leibniz H. Berihuete
+ * 
+ * Date Started: 6/1/2016 8:56 PM
+ * Last Modification: 6/1/2016 9:07 PM
+ * 
+ * StartMenuScene class
+ * This class is a scene that contains a layout that will show
+ * the start menu of this game. The purpose of this scene is to
+ * be used by a Stage class. * 
+ */
+public class StartMenuScene extends Scene {
+	private static final StartMenuLayout MENU_LAYOUT = new StartMenuLayout();
+	
+	/**
+	 * Constructor
+	 *  Initializes the scene with a StartMenuLayout that already contains
+	 *  the menu itself along with its menu buttons and functionalities.
+	 *  It also initializes the windows size in pixels.
+	 * @param windowWidth holds the windows' width
+	 * @param windowHeight holds the windows' height
+	 */
+	public StartMenuScene(int windowWidth, int windowHeight) {
+		super(MENU_LAYOUT, windowWidth, windowHeight);
+		
+	}
 }
--- game/scenes/layouts/StartMenuLayout.java
+++ game/scenes/layouts/StartMenuLayout.java
@@ -6,24 +6,50 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
+/**
+ * 
+ * @author Leibniz H. Berihuete
+ * 
+ * Date Started: 6/1/2016 8:01 PM
+ * Last Modification: 6/1/2016 8:56 PM
+ * 
+ * StartMenuLayout class
+ * This class contains a background image, and a menu that has a start-button, settings-button,
+ * and an exit-button. This class also extends to the Pane class, which is a container that
+ * can take any node and position it at coordinate (0,0) unless relocated.
+ */
+
 public class StartMenuLayout extends Pane {
+	// Background Image:
 	private static final Image BACKGROUND_IMG = 
 			new Image(StartMenuLayout.class.getResourceAsStream("../../images/startMenuBG.jpg"));
+	
+	// Background View -- in order to display the background:
 	private static final ImageView BACKGROUND = new ImageView(BACKGROUND_IMG);
 	
+/* ********************
+ * 	  CONSTRUCTORS
+ * ********************/
+	/**
+	 * Default constructor
+	 * It initializes the menu along with the menu buttons
+	 */
 	public StartMenuLayout() {
+		// Initialize menu buttons:
 		MenuButton startButton = new MenuButton("START", 200, 40);
 		MenuButton settingsButton = new MenuButton("SETTINGS");
 		MenuButton exitButton = new MenuButton("EXIT");
 		
-		
-		
+		// Reduce background image by 50%;
 		BACKGROUND.setFitWidth(BACKGROUND_IMG.getWidth()*0.50);
 		BACKGROUND.setFitHeight(BACKGROUND_IMG.getHeight()*0.50);
 		
-		VBox vbox = new VBox(startButton, settingsButton, exitButton);
-		vbox.setSpacing(5);
-		vbox.relocate(400-150, 300-40);
-		this.getChildren().addAll(BACKGROUND,vbox);
+		// Create Menu
+		VBox menu = new VBox(startButton, settingsButton, exitButton);
+		menu.setSpacing(5);
+		menu.relocate(250, 260);
+		
+		// Add background and menu to this container
+		this.getChildren().addAll(BACKGROUND, menu);
 	}
 }
